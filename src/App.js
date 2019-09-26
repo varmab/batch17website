4045d921bbfd7578ddedafc0998d239a0dd1ce72
Varma Bhupatiraju <varmab@gmail.com>
2019-09-26 12:17:05 +0530

    Initial version

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import HomePage from './containers/HomePage';
+import AboutPage from './containers/AboutPage';
+import ContactPage from './containers/ContactPage';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+import NotFoundPage from './containers/NotFoundPage';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage}/>
+          <Route path="/about" component={AboutPage}/>
+          <Route path="/contact" component={ContactPage}/>
+          <Route path="*" component={NotFoundPage}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
 
 export default App;
+    
